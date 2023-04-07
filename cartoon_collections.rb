cb7d0969c7db4abfb958c9c72ec9c4b342639817
Luna Seifert <thepuddingdeity@gmail.com>
2023-04-07 15:23:02 -0500

    commit tree 5e2b1dbadd19463934dec98d2d84e21130de61b4

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves names
+  names.each.with_index(1) {|name, index| puts "#{index}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet planeteer_calls
+  planeteer_calls.map do |call|
+    call[0].upcase + call.slice(1, planeteer_calls.length - 1) + "!"
+  end
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls calls
+  if calls.filter {|call| call.length > 4}.length > 0
+    true
+  else
+    false
+  end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese foods
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  foods.each do |food|
+    if cheese_types.include?(food)
+      return food
+    end
+  end
+  nil
 end
